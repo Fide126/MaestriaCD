bd6a529b4e75380c4f7b5a72b90b714b1e6d14e9
Fidel Villalba <126fide@gmail.com>
2025-07-08 15:46:33 -0600

    Lectura de dos archivos

--- datos.py
+++ datos.py
@@ -1,2 +1,6 @@
-ventas = pd.read_csv("./customer_data_2023.csv")
+import pandas as pd
+ventas = pd.read_csv("sales_data_2023.csv")
 print(ventas.head())
+
+clientes = pd.read_csv("customer_data_2023.csv")
+print(clientes.head())
