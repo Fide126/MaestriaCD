db29d82f5516901710bbf4874ff6cba88b67ef42
Fidel Villalba <126fide@gmail.com>
2025-07-10 08:23:18 -0600

    Actualizacion de trabajo

---
+++ analisisventas.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+productos = ['Cereal', 'Cafe', 'Anticongelante']
+ventas = [100, 150, 200]
+
+# Crear gráfico de barras
+
+plt.bar(productos, ventas)
+plt.title('Ventas por Producto')
+plt.show()
--- datos.py
+++ datos.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+
 ventas = pd.read_csv("sales_data_2023.csv")
 print(ventas.head())
 
